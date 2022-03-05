4746762ccbffeaf461a41d8a109cf62f8a4d28aa
Hao Chen <hao.chen4@student.tafesa.edu.au>
2022-03-05 23:54:54 +1030

    content page insert and update

--- conn/dbHelper.php
+++ conn/dbHelper.php
@@ -2,6 +2,7 @@
     include("./models/Route.php");
     include("./models/Content.php");
 
+    // route section
     function GetRoute() {
         require_once(__DIR__ . '/Conn.php');
         $q = "SELECT * FROM Route";
@@ -21,6 +22,7 @@
         return $routes;
     }
 
+    // page section
     function getPageByID($id) { 
         require_once(__DIR__ . '/Conn.php');
         $q = "SELECT * FROM content WHERE fkRouteID = $id";
@@ -43,9 +45,25 @@
     function insertNewPage($routeID, $page, $pageIndex, $pageContent) {
         require_once(__DIR__ . '/Conn.php');
         $q = "INSERT INTO Content (fkRouteID, Page, PageIndex, PageContent) VALUE ($routeID, '$page', $pageIndex, '$pageContent')";
-        return $q;
+        if (mysqli_query($conn, $q)) { 
+            mysqli_close($conn);
+            return true; 
+        }
+        mysqli_close($conn);
+        return false;
     }
 
+    function updatePage($contentID, $routeID, $page, $pageIndex, $pageContent) { 
+        require_once(__DIR__ . '/Conn.php');
+        $q = "UPDATE Content SET RouteID = $routeID, Page = '$page', PageIndex = $pageIndex, PageContent = '$pageContent' WHERE ContentID = $contentID";
+        if (mysqli_query($conn, $q)) {
+            mysqli_close($conn);
+            return true;
+        }
+        mysqli_close($conn);
+        return false;
+    }
 
+    // admin login section
 
 ?>
