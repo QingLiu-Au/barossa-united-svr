6ce19cddac65bb291d23dae1eaa5acd000d759ba
Qing Liu <qingliuhelen@gmail.com>
2022-03-26 23:55:26 +1030

    d

--- Config.php
+++ Config.php
@@ -25,10 +25,10 @@ function cors() {
     }
 
 }
+cors();
 
-
-// header("Access-Control-Allow-Origin: *");
 // header("Access-Control-Allow-Headers: 'X-Requested-With,content-type'");
+// header("Access-Control-Allow-Origin: *");
 // header("Access-Control-Allow-Methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE'");
 // header("Access-Control-Allow-Headers: *");
 
