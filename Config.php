11cc3842d65136bb0f9da51b64511ec3004efc91
Qing Liu <qingliuhelen@gmail.com>
2022-03-26 11:54:59 +1030

    update cros

--- Config.php
+++ Config.php
@@ -1,6 +1,17 @@
 <?php 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: *");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+    header('Access-Control-Allow-Headers: token, Content-Type');
+    header('Access-Control-Max-Age: 1728000');
+    header('Content-Length: 0');
+    header('Content-Type: text/plain');
+    die();
+}
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+// header("Access-Control-Allow-Headers: *");
 
 include $_SERVER['DOCUMENT_ROOT'].dirname($_SERVER['PHP_SELF']).'/conn/Conn.php';
 include $_SERVER['DOCUMENT_ROOT'].dirname($_SERVER['PHP_SELF']).'/conn/dbHelper.php';
