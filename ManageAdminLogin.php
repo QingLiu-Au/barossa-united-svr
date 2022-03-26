a5909ab1fa8c045544072722170913844ef597d2
Qing Liu <qingliuhelen@gmail.com>
2022-03-26 21:59:26 +1030

    d

--- ManageAdminLogin.php
+++ ManageAdminLogin.php
@@ -1,8 +1,8 @@
 <?php
     include 'Config.php';
-    $inputJSON = file_get_contents('php://input');
-    echo $inputJSON;
-    $arr = json_decode($inputJSON, true); //convert JSON into array
+    // $inputJSON = file_get_contents('php://input');
+    // echo $inputJSON;
+    $arr = json_decode(file_get_contents('php://input'), true);
     $isLogin = false;
     $email = '';
     $password = '';
@@ -10,19 +10,19 @@
     // Loop through the associative array
     foreach($arr as $key=>$value){
         //echo "key => $key, value => $value";
-        if ($key == 'isLogin') $isLogin = $value;
-        if ($key == 'email') $email = $value;
-        if ($key == 'password') $password = $value;
+        if ($key == "isLogin") $isLogin = $value;
+        if ($key == "email") $email = $value;
+        if ($key == "password") $password = $value;
     }
 
     session_start();
     if ($isLogin == 1) {
-        $_SESSION['email'] = $email;
-        $_SESSION['password'] = $password;
+        $_SESSION["email"] = $email;
+        $_SESSION["password"] = $password;
         echo true;
     }
     else {
-        if ($_SESSION['email'] == $email && $_SESSION['password'] == $password) {
+        if ($_SESSION["email"] == $email && $_SESSION["password"] == $password) {
             session_unset();
             session_destroy();
             echo true;
