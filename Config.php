26b68838c1f6443b0bd03f6bd5e19e2c829f79f7
Qing Liu <qingliuhelen@gmail.com>
2022-03-26 23:52:22 +1030

    d

--- Config.php
+++ Config.php
@@ -1,16 +1,35 @@
 <?php 
-// if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-//     header('Access-Control-Allow-Origin: *');
-//     header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
-//     header('Access-Control-Allow-Headers: *');
-//     header('Access-Control-Max-Age: 1728000');
-//     header('Content-Length: 0');
-//     header('Content-Type: text/plain');
-//     die();
-// }
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Headers: 'X-Requested-With,content-type'");
-header("Access-Control-Allow-Methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE'");
+
+function cors() {
+
+    // Allow from any origin
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
+        // you want to allow, and if so:
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            // may also be using PUT, PATCH, HEAD etc
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+        exit(0);
+    }
+
+}
+
+
+// header("Access-Control-Allow-Origin: *");
+// header("Access-Control-Allow-Headers: 'X-Requested-With,content-type'");
+// header("Access-Control-Allow-Methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE'");
 // header("Access-Control-Allow-Headers: *");
 
 include $_SERVER['DOCUMENT_ROOT'].dirname($_SERVER['PHP_SELF']).'/conn/Conn.php';
