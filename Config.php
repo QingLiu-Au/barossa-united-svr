c519a6c7e804cbc5f7ea381f459f9ff630ee689f
Qing Liu <qingliuhelen@gmail.com>
2022-03-26 12:03:44 +1030

    cros

--- Config.php
+++ Config.php
@@ -1,16 +1,16 @@
 <?php 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
-    header('Access-Control-Allow-Headers: *');
-    header('Access-Control-Max-Age: 1728000');
-    header('Content-Length: 0');
-    header('Content-Type: text/plain');
-    die();
-}
-
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
+// if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+//     header('Access-Control-Allow-Origin: *');
+//     header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+//     header('Access-Control-Allow-Headers: *');
+//     header('Access-Control-Max-Age: 1728000');
+//     header('Content-Length: 0');
+//     header('Content-Type: text/plain');
+//     die();
+// }
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: 'X-Requested-With,content-type'");
+header("Access-Control-Allow-Methods: 'GET, POST, OPTIONS, PUT, PATCH, DELETE'");
 // header("Access-Control-Allow-Headers: *");
 
 include $_SERVER['DOCUMENT_ROOT'].dirname($_SERVER['PHP_SELF']).'/conn/Conn.php';
