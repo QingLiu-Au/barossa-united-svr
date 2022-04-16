abaf6abae0f91ddd0a1717a755b4ce2e6e62fa6f
Qing Liu <qingliuhelen@gmail.com>
2022-04-16 10:50:53 +0930

    get admin user

--- GetAdminUser.php
+++ GetAdminUser.php
@@ -1,32 +1,6 @@
 <?php
     include "Config.php";
-    $arr = json_decode(file_get_contents("php://input"), true);
-    $isLogin = false;
-    $email = "";
-    $password = "";
 
-    // Loop through the associative array
-    foreach($arr as $key=>$value){
-        // echo "key => $key, value => $value";
-        if ($key == "isLogin") $isLogin = $value;
-        if ($key == "email") $email = $value;
-        if ($key == "password") $password = $value;
-    }
-
-    if (session_status() == PHP_SESSION_NONE) {
-        session_start();
-    }
-    if (isset($_SESSION['email']) && isset($_SESSION['password'])) {
-        if ($_SESSION["email"] = $email && $_SESSION["password"] = $password) {
-            $users = GetAllUsers($conn);
-            echo json_encode($users);
-        } else {
-            echo "[]";
-        }
-    } else {
-        echo "[]";
-    }
-    
-    
-
+    $users = GetAllUsers($conn);
+    echo json_encode($users);
 ?>
